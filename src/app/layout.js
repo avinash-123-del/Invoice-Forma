e13b6331205665cb4fd7db68646a3b51d2508e62
avinash-123-del <avichandraker@gmail.com>
2023-08-22 23:49:19 +0530

    firsts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-'use client'
-import AppContextProvider from './Components/ContextProvider'
+
+import AppProvider from './Appprovider/AppProvider'
 import './globals.css'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppContextProvider>
+        <AppProvider>
           {children}
-        </AppContextProvider>
+        </AppProvider>
       </body>
     </html>
   )
